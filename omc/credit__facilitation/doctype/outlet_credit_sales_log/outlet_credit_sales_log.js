9ae9ecd6795703973ca7670ff9f45576ef72d6db
kelvin <otuahenedev@gmail.com>
2024-11-17 22:28:43 +0000

    fixed issuess regarding fetching staff names and add provision of fetching product prices from linked outlet

--- omc/credit__facilitation/doctype/outlet_credit_sales_log/outlet_credit_sales_log.js
+++ omc/credit__facilitation/doctype/outlet_credit_sales_log/outlet_credit_sales_log.js
@@ -7,7 +7,50 @@
 // 	},
 // });
 frappe.ui.form.on("Outlet Credit Sales Log", {
-    refresh: function(frm){
+    // fetch pricing from linked outlet
+    outlet: function(frm) {
+        let outlet = frm.doc.outlet;
+        frm.clear_table("dispenser_readings");
+        
+        if (!outlet) {
+            frappe.msgprint(__('Please select an Outlet.'));
+            return;
+        }
+        
+        // Fetch product pricing data from Customer doctype
+        frappe.call({
+            method: "frappe.client.get",
+            args: {
+                doctype: "Customer",
+                name: outlet,
+            },
+            callback: function(response) {
+                let customer = response.message;
+                
+                // Clear and populate 'product_pricing' table
+                if (customer && customer.custom_product_rates) {
+                    frm.clear_table("product_pricing");
+                    customer.custom_product_rates.forEach(product => {
+                        let child = frm.add_child("product_pricing");
+                        child.product = product.product;
+                        child.rate = product.rate;
+                    });
+                    frm.refresh_field("product_pricing");
+                    
+                } else {
+                    frappe.msgprint(__('No products found for the selected Outlet.'));
+                }
+                
+              
+            },
+            error: function() {
+                frappe.msgprint(__('Failed to fetch Outlet data.'));
+            }
+        });
+    },
+    // fetch staff from company approved to have credit
+    company: function(frm){
+        const company = frm.doc.company;
         frappe.call({
             method: "omc.credit__facilitation.doctype.outlet_credit_sales_log.outlet_credit_sales_log.staffdetails",
             args: { staff: company },  // Pass 'staff' argument for staff details
@@ -33,7 +76,7 @@ function update_staff_select_options(frm, staff_data) {
     let staffing = Array.isArray(staff_data.message) ? staff_data.message : Object.values(staff_data.message);
 
     // Update 'staff' field options dynamically for both child tables
-    frm.fields_dict.dispenser_readings.grid.update_docfield_property('name1', "options", staffing);
+    frm.set_df_property("name1", "options", staffing.join("\n"));
    
     // Refresh fields to apply updated staff options
    frm.refresh_field("name1");
